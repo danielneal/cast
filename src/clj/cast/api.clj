6884460975ba829bf579216195bd6a48ed79830b
Daniel Neal <danielneal2@gmail.com>
2014-06-09 15:43:42 +0100

    Add logging in functionality (client-side only so far!)

--- src/clj/cast/api.clj
+++ src/clj/cast/api.clj
@@ -2,37 +2,4 @@
   (:require [tailrecursion.castra :refer [defrpc]]
             [cast.db :as db]))
 
-(#'clojure.core/load-data-readers)
-
-(def features [{:title "core.async"
-               :description "asynchronous programming using channels"}
-              {:title "core.logic"
-               :description "embedded logic language like Prolog"}
-              {:title "core.typed"
-               :description "gradual typing for clojure"}
-              {:title "datomic"
-               :description "immutable database with time support and datalog query language"}
-              {:title "instaparse"
-               :description "make grammar-based parsing as easy to use as regexes"}
-              {:title "clojure-sql and yesql"
-               :description "my two favourite database access libraries"}
-              {:title "hoplon"
-               :description "Clojure/ClojureScript web framework based on spreadsheet-like cells and HLisp"}
-              {:title "core.match"
-               :description "pattern matching as a library"}
-              {:title "overtone"
-               :description "clojure wrapper for supercollider (making noises)"}])
-
-(def all-votes (atom {}))
-
-(defrpc get-state []
-  {:features features
-   :all-votes @all-votes})
-
-(defrpc vote! [title direction]
-  (swap! all-votes update-in [title]
-         (case direction
-           :up (fnil inc 0)
-           :down dec))
-  (get-state))
-
+(defrpc get-state [])
--- src/clj/cast/db.clj
+++ src/clj/cast/db.clj
@@ -6,51 +6,42 @@
 ;;  Database Setup
 ;; -------------------------------
 
-(def uri "datomic:mem://feature-list")
-(d/create-database uri)
+(defn init! [uri]
+  (let [schema [{:db/id (d/tempid :db.part/db)
+                 :db/ident :feature/title
+                 :db/valueType :db.type/string
+                 :db/cardinality :db.cardinality/one
+                 :db/doc "The title of a feature"
+                 :db.install/_attribute :db.part/db}
 
-(def schema
-  [{:db/id (d/tempid :db.part/db)
-    :db/ident :feature/title
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/doc "The title of a feature"
-    :db.install/_attribute :db.part/db}
+                {:db/id (d/tempid :db.part/db)
+                 :db/ident :feature/description
+                 :db/valueType :db.type/string
+                 :db/cardinality :db.cardinality/one
+                 :db/doc "The description of a feature"
+                 :db.install/_attribute :db.part/db}
 
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :feature/description
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/doc "The description of a feature"
-    :db.install/_attribute :db.part/db}
+                {:db/id (d/tempid :db.part/db)
+                 :db/ident :feature/votes
+                 :db/valueType :db.type/long
+                 :db/cardinality :db.cardinality/one
+                 :db/doc "The name of votes the feature has received"
+                 :db.install/_attribute :db.part/db}
 
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :feature/votes
-    :db/valueType :db.type/long
-    :db/cardinality :db.cardinality/one
-    :db/doc "The name of votes the feature has received"
-    :db.install/_attribute :db.part/db}
+                {:db/id (d/tempid :db.part/db)
+                 :db/ident :feature/id
+                 :db/unique :db.unique/identity
+                 :db/valueType :db.type/uuid
+                 :db/cardinality :db.cardinality/one
+                 :db/doc "A unique id for a feature"
+                 :db.install/_attribute :db.part/db}]
+        conn (d/connect uri)]
+    (d/create-database uri)
+    (d/transact conn schema)
+    conn))
 
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :feature/id
-    :db/unique :db.unique/identity
-    :db/valueType :db.type/uuid
-    :db/cardinality :db.cardinality/one
-    :db/doc "A unique id for a feature"
-    :db.install/_attribute :db.part/db}])
+(def conn (init! "datomic:mem://cast"))
 
-(let [conn (d/connect uri)]
-  (d/transact conn schema)) ;; FIX THIS: (-> "cast/schema.edn" clojure.java.io/resource slurp read-string)))
-
-;; -------------------------------
-;;  Database Helpers
-;; -------------------------------
-
-(defn latest
-  "Returns the latest database"
-  []
-  (let [conn (d/connect uri)]
-    (d/db conn)))
 
 ;; -------------------------------
 ;;  Queries
@@ -64,18 +55,4 @@
             :where [?e :feature/id ?f]] db)
        (map #(d/touch (d/entity db (first %))))
        (into [])))
-
-;; -------------------------------
-;;  Commands
-;; -------------------------------
-
-(defn add-feature
-  "Add a feature to the database"
-  [feature]
-  (let [feature (assoc feature
-                  :feature/id (d/squuid)
-                  :db/id (d/tempid :db.part/user))
-        conn (d/connect uri)]
-    (d/transact conn [(assoc feature :feature/id (d/squuid) :db/id (d/tempid :db.part/user))])))
-
 
