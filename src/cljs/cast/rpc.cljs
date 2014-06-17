4e385e70478b9181f5732dfd82831dc2a191ec76
Daniel Neal <danielneal2@gmail.com>
2014-06-17 15:30:56 +0100

    Back to using Castra now

--- src/cljs/cast/rpc.cljs
+++ src/cljs/cast/rpc.cljs
@@ -9,9 +9,10 @@
 (defc error nil)
 (defc loading [])
 
-(def get-entities
-  (mkremote 'cast.api/get-entities entities error loading))
-
-(def transact!
-  (mkremote 'cast.api/transact! entities error loading))
-
+(def command
+  {:get-entities (mkremote 'cast.api/get-entities entities error loading)
+   :up-vote (mkremote 'cast.api/up-vote entities error loading)
+   :down-vote (mkremote 'cast.api/down-vote entities error loading)
+   :add-feature (mkremote 'cast.api/add-feature entities error loading)
+   :login (mkremote 'cast.api/login entities error loading)
+   :logout (mkremote 'cast.api/logout entities error loading)})
