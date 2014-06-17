6d411cfc687e3aec403b51009ef15e668116f509
Daniel Neal <danielneal2@gmail.com>
2014-06-17 15:30:18 +0100

    Clean up api

--- src/clj/cast/api.clj
+++ src/clj/cast/api.clj
@@ -8,11 +8,25 @@
         attributes [:feature/title :user/name :vote/user :page/name]]
     (mapcat #(cast-db/load-entities db (cast-db/all-with-attribute db %)) attributes)))
 
-(defn resolve-ids [tx-data]
-  (clojure.walk/postwalk (fn [e] (if (and (number? e) (neg? e))
-                                    (d/tempid :db.part/user e)
-                                   e)) tx-data))
-(defrpc transact! [tx-data]
-  (let [processed-tx-data (resolve-ids tx-data)]
-    (d/transact cast-db/conn processed-tx-data)
-    (get-entities)))
+(defrpc up-vote [feature user]
+  (d/transact cast-db/conn [{:db/id (d/tempid :db.part/user) :vote/feature feature :vote/user user}])
+  (get-entities))
+
+(defrpc down-vote [feature user]
+  (when-let [v (ffirst (d/q '[:find ?v
+                        :in $ ?f ?u
+                        :where
+                        [?v :vote/feature ?f]
+                        [?v :vote/user ?u]]
+                      (d/db cast-db/conn) feature user))]
+    (d/transact cast-db/conn [[:db.fn/retractEntity v]]))
+  (get-entities))
+
+(defrpc add-feature [title description]
+  (get-entities))
+
+(defrpc login [username password]
+  (get-entities))
+
+(defrpc logout [username]
+  (get-entities))
